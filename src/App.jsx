a202810577a0059a2de1f95ecbbb431a4c39b38f
Andrey Emelianov <621077@mail.ru>
2023-09-03 17:34:42 +0300

    начал создание компонента кнопки

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Button></Button>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,6 @@
+import './Button.css';
+
+const Button = () => {
+	return <button className="button accent">Сохранить</button>;
+};
+export default Button;
