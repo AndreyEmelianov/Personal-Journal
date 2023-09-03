18fa40f99c49e4943e7257511891fcf5372a7621
Andrey Emelianov <621077@mail.ru>
2023-09-03 19:40:04 +0300

    сделал заготовку файлов для компонентов

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import './Button.css';
 
 const Button = () => {
-	return <button className="button accent">Сохранить</button>;
+	const [text, setText] = useState('Сохранить');
+
+	const clicked = () => {
+		setText('поменял текст');
+	};
+
+	return (
+		<button onClick={clicked} className="button accent">
+			{text}
+		</button>
+	);
 };
 export default Button;
